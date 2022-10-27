3933340c7f1de5e1c6ed73f412de34f091475d83
deabhinav <116711900+deabhinav@users.noreply.github.com>
2022-10-27 14:42:57 +0530

    Program_to_generate_Pascal’s_Triangle.cpp
    Problem Statement: Given an integer N, return the first N rows of Pascal’s triangle.

---
+++ Program_to_generate_Pascal’s_Triangle.cpp
@@ -0,0 +1,37 @@
+// Problem Statement: Given an integer N, return the first N rows of Pascal’s triangle.
+
+// Example 1:
+
+// Input Format: N = 5
+
+// Result:
+//     1
+//    1 1
+//   1 2 1
+//  1 3 3 1
+// 1 4 6 4 1
+
+// Explanation: There are 5 rows in the output matrix. Each row corresponds to each one of the rows in the image shown above.
+// Example 2:
+
+// Input Format: N = 1
+
+// Result: 
+//     1
+
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> r(numRows);
+
+        for (int i = 0; i < numRows; i++) {
+            r[i].resize(i + 1);
+            r[i][0] = r[i][i] = 1;
+  
+            for (int j = 1; j < i; j++)
+                r[i][j] = r[i - 1][j - 1] + r[i - 1][j];
+        }
+        
+        return r;
+    }
+};
